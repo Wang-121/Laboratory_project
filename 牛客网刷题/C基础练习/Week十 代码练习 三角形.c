eb9b418dcd4b4bebc1089a74c46ecc379d9f8056
Wang-121 <66828414+Wang-121@users.noreply.github.com>
2020-11-07 18:47:04 +0800

    Add files via upload

---
+++ 牛客网刷题/C基础练习/Week十 代码练习 三角形.c
@@ -0,0 +1,18 @@
+//直角三角形图案
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    while(scanf("%d",&i)!=EOF)
+    {
+        for(j=1;j<=i;j++)
+        {
+            for(k=1;k<=j;k++)
+             printf("* ");
+            printf("\n");
+        }
+
+    }
+
+    return 0;
+}
