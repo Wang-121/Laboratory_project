2a59d27f3fc91de827584b359cce519bc77eb6e7
Wang-121 <66828414+Wang-121@users.noreply.github.com>
2020-11-19 16:40:59 +0800

    Add files via upload

---
+++ 牛客网刷题/C基础练习/Week十一 代码练习 X形.c
@@ -0,0 +1,25 @@
+//XÐÎÍ¼°¸
+#include<stdio.h>
+int main()
+{
+    int n;
+    while(scanf("%d",&n)!=EOF)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j||i==n-1-j)
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
