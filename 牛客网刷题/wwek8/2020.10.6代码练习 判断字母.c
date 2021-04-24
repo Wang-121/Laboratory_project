2e4e91128fc1790825eac41723bbf7bbaa09a50d
Wang-121 <66828414+Wang-121@users.noreply.github.com>
2021-04-24 17:17:27 +0800

    Add files via upload

---
+++ 牛客网刷题/wwek8/2020.10.6代码练习 判断字母.c
@@ -0,0 +1,13 @@
+//еп╤ойг╡╩йгвжд╦
+#include <stdio.h>
+int main(){
+    char c;
+    while(scanf("%c",&c)!=EOF){
+        if(c>='A'&&c<='z')
+            printf("%c is an alphabet.\n",c);
+        else
+            printf("%c is not an alphabet.\n",c);
+        getchar();
+    }
+    return 0;
+}
