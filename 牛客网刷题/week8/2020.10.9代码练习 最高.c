03d33705155b65539b9592a2427557093a4ea68c
Wang-121 <66828414+Wang-121@users.noreply.github.com>
2021-04-24 17:18:12 +0800

    Add files via upload

---
+++ 牛客网刷题/week8/2020.10.9代码练习 最高.c
@@ -0,0 +1,13 @@
+//最高分数
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    while(scanf("%d %d %d",&a,&b,&c)!=EOF)
+    {
+        int m =a>b?a:b;
+        m=m>c?m:c;
+        printf("%d\n",m);
+    }
+    return 0;
+}
